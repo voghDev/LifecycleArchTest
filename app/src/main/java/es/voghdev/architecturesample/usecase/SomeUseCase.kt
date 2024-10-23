9c6ecc0dc06492da9c51bdd692545c39f755484f
Olmo Gallegos <olmo.gallegos@ext.domestika.org>
2024-10-23 17:23:51 +0200

    feat: Add use case

--- app/src/main/java/es/voghdev/architecturesample/usecase/SomeUseCase.kt
+++ app/src/main/java/es/voghdev/architecturesample/usecase/SomeUseCase.kt
@@ -1,3 +1,12 @@
 package es.voghdev.architecturesample.usecase
 
-class SomeUseCase
+import io.reactivex.rxjava3.core.Single
+import java.util.concurrent.TimeUnit
+
+class SomeUseCase {
+    operator fun invoke() : Single<Result<Unit>> =
+        Single.just(Unit)
+            .delay(3000, TimeUnit.MILLISECONDS)
+            .dispatch()
+            .toResult()
+}
--- app/src/main/java/es/voghdev/architecturesample/viewmodel/MainViewModel.kt
+++ app/src/main/java/es/voghdev/architecturesample/viewmodel/MainViewModel.kt
@@ -2,12 +2,21 @@ package es.voghdev.architecturesample.viewmodel
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import es.voghdev.architecturesample.usecase.Result
 import es.voghdev.architecturesample.usecase.SomeUseCase
 import es.voghdev.architecturesample.viewstate.MainViewState
 
 class MainViewModel(
     private val someUseCase: SomeUseCase
-): BaseViewModel() {
+) : BaseViewModel() {
     private val _viewState = MutableLiveData<MainViewState>(MainViewState.Loading)
     val viewState: LiveData<MainViewState> = _viewState
+
+    init {
+        addToDisposable(someUseCase.invoke().subscribe(::handleUseCaseResult))
+    }
+
+    private fun handleUseCaseResult(result: Result<Unit>) {
+        _viewState.value = MainViewState.ContentLoaded
+    }
 }
